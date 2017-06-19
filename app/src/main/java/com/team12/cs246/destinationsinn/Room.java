5e696c2115bad2f181a9db38aac188fad72f10ad
jasonchio <chi12017@byui.edu>
2017-06-19 13:03:33 -0600

    klasdjfklajksdflas;jf

--- app/src/main/java/com/team12/cs246/destinationsinn/Room.java
+++ app/src/main/java/com/team12/cs246/destinationsinn/Room.java
@@ -3,6 +3,7 @@ package com.team12.cs246.destinationsinn;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class Room extends AppCompatActivity {
 
@@ -10,7 +11,10 @@ public class Room extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
+
         String welcome = intent.getStringExtra("welcome");
+        TextView tv = (TextView) findViewById(R.id.newYork);
+        setContentView(tv);
         setTitle(welcome);
         setContentView(R.layout.activity_room);
     }
