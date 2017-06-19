2950357de5c1413ca09ed581534f324b1b91850b
fizhy37 <fis15028@byui.edu>
2017-06-19 13:27:26 -0600

    Started room.xml layout

--- app/src/main/java/com/team12/cs246/destinationsinn/Destinations.java
+++ app/src/main/java/com/team12/cs246/destinationsinn/Destinations.java
@@ -18,47 +18,49 @@ public class Destinations extends AppCompatActivity {
 
     public void openAlaska(View view) {
         Intent i = new Intent(this, Room.class);
-        i.putExtra("welcome", R.string.alaska);
+        i.putExtra("welcome", getString(R.string.alaska));
         startActivity(i);
     }
 
     public void openArabia(View view) {
         Intent i = new Intent(this, Room.class);
-        i.putExtra("welcome", R.string.arabia);
-        i.putExtra("fname", "Ben");
-        i.putExtra("lname", "Fisher");
+        i.putExtra("welcome", getString(R.string.arabia));
         startActivity(i);
     }
 
     public void openAthens(View view) {
         Intent i = new Intent(this, Room.class);
-        i.putExtra("welcome", R.string.athens);
+        i.putExtra("welcome", getString(R.string.athens));
         startActivity(i);
     }
 
     public void openCongo(View view) {
         Intent i = new Intent(this, Room.class);
-        i.putExtra("welcome", R.string.congo);
+        i.putExtra("welcome", getString(R.string.congo));
         startActivity(i);
     }
 
     public void openEgypt(View view) {
         Intent i = new Intent(this, Room.class);
+        i.putExtra("welcome", getString(R.string.egypt));
         startActivity(i);
     }
 
     public void openEngland(View view) {
         Intent i = new Intent(this, Room.class);
+        i.putExtra("welcome", getString(R.string.england));
         startActivity(i);
     }
 
     public void openHawaii(View view) {
         Intent i = new Intent(this, Room.class);
+        i.putExtra("welcome", getString(R.string.hawaii));
         startActivity(i);
     }
 
     public void openMorocco(View view) {
         Intent i = new Intent(this, Room.class);
+        i.putExtra("welcome", getString(R.string.morocco));
         startActivity(i);
     }
 
@@ -70,11 +72,13 @@ public class Destinations extends AppCompatActivity {
 
     public void openParis(View view) {
         Intent i = new Intent(this, Room.class);
+        i.putExtra("welcome", getString(R.string.paris));
         startActivity(i);
     }
 
     public void openRio(View view) {
         Intent i = new Intent(this, Room.class);
+        i.putExtra("welcome", getString(R.string.rio));
         startActivity(i);
     }
 
@@ -86,11 +90,13 @@ public class Destinations extends AppCompatActivity {
 
     public void openThailand(View view) {
         Intent i = new Intent(this, Room.class);
+        i.putExtra("welcome", getString(R.string.thailand));
         startActivity(i);
     }
 
     public void openVenice(View view) {
         Intent i = new Intent(this, Room.class);
+        i.putExtra("welcome", getString(R.string.venice));
         startActivity(i);
     }
 }
