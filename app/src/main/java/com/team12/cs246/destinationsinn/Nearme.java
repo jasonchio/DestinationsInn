b72ddb21bcf6dcd6750f727a93078dca69532bab
jasonchio <chi12017@byui.edu>
2017-07-04 19:36:02 -0600

    akjfaklsdjf;l

--- app/src/main/java/com/team12/cs246/destinationsinn/Nearme.java
+++ app/src/main/java/com/team12/cs246/destinationsinn/Nearme.java
@@ -1,14 +1,16 @@
 package com.team12.cs246.destinationsinn;
 
 import android.location.Location;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+//import com.google.android.gms.location.FusedLocationPrvi;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,14 +18,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Nearme extends FragmentActivity implements OnMapReadyCallback {
+public class Nearme extends FragmentActivity implements OnMapReadyCallback,LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    protected GoogleApiClient mGoogleApiClient;
+  //  private FusedLocationProviderClient mFusedLocationClient;
+    Location mLastLocation;
+    double lat =0, lng=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearme);
+        buildGoogleApiClient();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -44,11 +53,13 @@ public class Nearme extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+    //    mMap.setMyLocationEnabled(true);
+
 
         // Add a marker in Sydney and move the camera
-        LatLng blackswan = new LatLng(42.8680932, -112.4427292);
-        mMap.addMarker(new MarkerOptions().position(blackswan).title("You are here."));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(blackswan));
+       LatLng blackswan = new LatLng(lat, lng);
+       mMap.addMarker(new MarkerOptions().position(blackswan).title("You are here."));
+       mMap.moveCamera(CameraUpdateFactory.newLatLng(blackswan));
 
         LatLng desination = new LatLng(43.491045, -112.0425047);
         mMap.addMarker(new MarkerOptions().position(desination).title("You are here."));
@@ -57,4 +68,84 @@ public class Nearme extends FragmentActivity implements OnMapReadyCallback {
         mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(42.8680932, -112.4427292) , 17.0f) );
     }
 
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
+
+
+/*
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            lat = mLastLocation.getLatitude();
+            lng = mLastLocation.getLongitude();
+
+            LatLng loc = new LatLng(lat, lng);
+            mMap.addMarker(new MarkerOptions().position(loc).title("New Marker"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+        }
+    }
+*/
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+
+            dLat = mLastLocation.getLatitude();
+
+
+            dLong = mLastLocation.getLongitude();
+        } else {
+            Toast.makeText(this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+}
