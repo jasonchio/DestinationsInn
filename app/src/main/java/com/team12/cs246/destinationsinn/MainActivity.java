0645346497f3a440ea42bcf27b8bee11feb02851
jasonchio <chi12017@byui.edu>
2017-07-14 11:14:24 -0600

    changing our mistake

--- app/src/main/java/com/team12/cs246/destinationsinn/MainActivity.java
+++ app/src/main/java/com/team12/cs246/destinationsinn/MainActivity.java
@@ -5,61 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener     {
-    private Button login, register;
-    private EditText etEmail, etPass;
-    private DbHelper db;
-    private Session session;
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(R.string.main);
         setContentView(R.layout.activity_main);
-
-        db = new DbHelper(this);
-        session = new Session(this);
-        login = (Button)findViewById(R.id.btnLogin);
-        register = (Button)findViewById(R.id.btnReg);
-        etEmail = (EditText)findViewById(R.id.etEmail);
-        etPass = (EditText)findViewById(R.id.etPass);
-        login.setOnClickListener(this);
-        register.setOnClickListener(this);
-
-        if(session.loggedin()){
-            startActivity(new Intent(MainActivity.this,select_hotel.class));
-            finish();
-        }
     }
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.btnLogin:
-                login();
-                break;
-            case R.id.btnReg:
-                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
-                break;
-            default:
-
-        }
+    public void openDestinations(View view) {
+        Intent i = new Intent(this, Destinations.class);
+        startActivity(i);
     }
 
-    private void login(){
-        String email = etEmail.getText().toString();
-        String pass = etPass.getText().toString();
-
-        if(db.getUser(email,pass)){
-            session.setLoggedin(true);
-            startActivity(new Intent(MainActivity.this, select_hotel.class));
-            finish();
-        }else{
-            Toast.makeText(getApplicationContext(), "Wrong email/password",Toast.LENGTH_SHORT).show();
-        }
+    public void openBlackSwan(View view) {
+        Intent i = new Intent(this, BlackSwan.class);
+        i.putExtra("welcome", R.string.app_name);
+        startActivity(i);
     }
-
-
-
 }
